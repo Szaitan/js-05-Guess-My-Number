af5e386a53ad22e38ae849eba7b7733c5543467a
michal.krepiniewicz <m.krepiniewicz@gmail.com>
2024-10-30 18:37:00 +0100

    Finished project

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
-let numberToFound = Math.floor(Math.random()*(20-1)+1)
+let numberToFound = Math.floor((Math.random()*20)+1)
+let score = 20
 console.log(numberToFound)
 
 // How to select element from document: document.QuerySelector
@@ -12,26 +13,45 @@ console.log(document.querySelector(".message").textContent)
 console.log(document.querySelector(".message").value)
 
 function checkNumber() {
-    const numberToCheck = Number(document.querySelector(".guess").value)
-    if (Number(document.querySelector(".score").textContent) == 0) {
-        document.querySelector(".message").textContent = "You lost. Try again."
-        document.body.style.backgroundColor = "red"
-    }   else  if (numberToCheck === numberToFound) {
-        document.querySelector(".message").textContent = "That is correct number!"
-        document.querySelector(".highscore").textContent = document.querySelector(".score").textContent
-        document.body.style.backgroundColor = "green"
-    }  else if (numberToCheck < numberToFound) {
-        document.querySelector(".message").textContent = "📉 To Low. Try again."
-        document.querySelector(".score").textContent = Number(document.querySelector(".score").textContent) - 1
-    } else if (numberToCheck > numberToFound) {
-        document.querySelector(".message").textContent = "📈 To High. Try again."
-        document.querySelector(".score").textContent = Number(document.querySelector(".score").textContent) - 1
+    if (!document.querySelector(".guess").value)
+        {document.querySelector(".message").textContent = "Please chose a number."} else {
+            const numberToCheck = Number(document.querySelector(".guess").value)
+            if (numberToCheck === numberToFound) {
+                document.querySelector(".message").textContent = "That is correct number!"
+                if (score > Number(document.querySelector('.highscore').textContent)) document.querySelector('.highscore').textContent = score
+                document.body.style.backgroundColor = "green"
+            }  
+            
+            else if (numberToCheck < numberToFound) {
+                document.querySelector(".message").textContent = "📉 To Low. Try again."
+                score--
+                document.querySelector(".score").textContent = score
+            } 
+            
+            else if (numberToCheck > numberToFound) {
+                document.querySelector(".message").textContent = "📈 To High. Try again."
+                score--
+                document.querySelector(".score").textContent = score
+            }
+            
+            if (Number(document.querySelector(".score").textContent) == 0) {
+                document.querySelector(".message").textContent = "You lost. Try again."
+                document.body.style.backgroundColor = "red"
+            }      
+        }
     }
-}
 
 function resetGame() {
-    document.querySelector(".score").textContent = 20
+    score = 20
+    document.querySelector(".score").textContent = score
     document.querySelector(".message").textContent = "Start guessing..."
     document.querySelector(".guess").value = ""
-    numberToFound = Math.floor(Math.random()*(20-1)+1)
+    numberToFound = Math.floor((Math.random()*20)+1)
+    console.log(numberToFound)
+    document.body.style.backgroundColor = "grey"
 }
+
+// Creation of event listener
+document.querySelector(".check").addEventListener("click", checkNumber)
+
+document.querySelector(".again").addEventListener("click", resetGame)
